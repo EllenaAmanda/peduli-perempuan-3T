1a5ed8b16ed6f6c944e5852683f5448a9b44fa6b
EllenaAmanda <ellenaamanda846@gmail.com>
2024-10-04 15:51:14 +0700

    get API

--- src/components/ListHome.jsx
+++ src/components/ListHome.jsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react"
 import CardHome from "./CardHome"
 
 function ListHome() {
+    
+    const [dataPerempuan, setDataPerempuan] = useState([]);
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = async () => {
+        try {
+            let URL = "https://66ff649b2b9aac9c997f225a.mockapi.io/dataperempuan3t";
+            const response = await fetch(URL, { 
+                method: 'GET',
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+              })
+            const result = await response.json()
+
+            setDataPerempuan(result)
+        }catch(e){
+            console.log(e)
+        }
+    }
+    // console.log(dataPerempuan)
+
   return (
     <div>
         <section className="py-16 ">
